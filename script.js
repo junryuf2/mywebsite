d3da77753775dfeebb060f1323ecb8887ecec26f
junryuf2 <junryuf2@illinois.edu>
2024-12-30 19:32:32 -0600

    removed top bar and added gradient background

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Website is ready!");
 });
+
+function showSection(sectionId) {
+    const allSections = document.querySelectorAll('.tab-content');
+    allSections.forEach((section) => {
+        section.classList.remove('active');
+        section.style.display = 'none';
+    });
+    
+    const selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.add('active');
+    selectedSection.style.display = 'block';
+}
